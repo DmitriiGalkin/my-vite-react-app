0944be5adcc93edff4f5ae5be1fda091995adc2f
Дмитрий <4757037@gmail.com>
2026-05-14 23:31:08 +0300

    pwa 2

--- application/public/sw.js
+++ application/public/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'quantum-pwa-v1';
 
-const STATIC_ASSETS = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
+const STATIC_ASSETS = ['/', '/manifest.webmanifest', '/icons/192.png', '/icons/512.png'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
